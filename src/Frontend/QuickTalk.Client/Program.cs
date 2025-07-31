564b17789375cb15ef0464e9eb675ecd4d567607
pablo <git-pablo@github.com>
2025-08-01 01:46:34 +0500

    added some mudblazor

--- src/Frontend/QuickTalk.Client/Program.cs
+++ src/Frontend/QuickTalk.Client/Program.cs
@@ -2,6 +2,7 @@ using QuickTalk.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QuickTalk.Client.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -18,4 +19,5 @@ builder.Services.Configure<ApiBaseAddressConfiguration>(e =>
     => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 builder.Services.AddScoped<ApiMessageService>();
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
